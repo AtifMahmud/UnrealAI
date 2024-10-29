2e2b798015b6e5178ae352d5d83d29a21fd56df8
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2024-10-28 22:58:34 -0700

    Initial commit

---
+++ Source/UnrealAIProject/Private/AIChatBot.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIChatBot.h"
+
+AIChatBot::AIChatBot()
+{
+}
+
+AIChatBot::~AIChatBot()
+{
+}
---
+++ Source/UnrealAIProject/Public/AIChatBot.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UNREALAIPROJECT_API AIChatBot
+{
+public:
+	AIChatBot();
+	~AIChatBot();
+};
